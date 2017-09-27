f0c4878625cfa9f5994752e9a6c31f3519317b17
plylrnsdy <plylrnsdy@163.com>
2017-09-27 23:36:07 +0800

    Fix configuration description spelling problem. New a TODOs files.

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,6 @@ import config from './config'
 import Runner from './Runner'
 import Terminal from './Terminal'
 
-// TODO: 国际化
-// TODO: 使用 $ 表示在非打开新终端窗口模式中将该命令在新终端窗口中打开
-
 export function activate(context: vscode.ExtensionContext) {
 
     let runner = new Runner(config, new Terminal('Run as ...'))
